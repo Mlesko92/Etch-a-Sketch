3207175aef50691b7b478eb93eb2b09b9145f5bc
Wizqk <leskom92@gmail.com>
2024-10-31 17:13:23 +0100

    added fancy css change

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ button.textContent = "Change the size";
 
 body.insertBefore(button, content);
 
+function getRandomRGB() {
+    const r = Math.floor(Math.random() * 256); // Random red value (0-255)
+    const g = Math.floor(Math.random() * 256); // Random green value (0-255)
+    const b = Math.floor(Math.random() * 256); // Random blue value (0-255)
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
 function createSquares(numberPerRow) {
   const total = numberPerRow * numberPerRow + numberPerRow;
   const lineBreaker = numberPerRow + 1;
@@ -16,14 +23,10 @@ function createSquares(numberPerRow) {
       square.style.cssText = "height: 0; width: 0; width: 100%";
     } else {
       square.style.cssText = "height: 48px; width: 48px;";
-      const squares = document.querySelectorAll(".content div");
-      
       // hover effect
-      squares.forEach((element) => {
-        element.addEventListener("mouseenter", () => {
-          element.style.backgroundColor = "blue";
-        });
-      });
+      square.addEventListener("mouseenter", () => {
+        square.style.backgroundColor = getRandomRGB();
+      })
     }
     content.appendChild(square);
   }
